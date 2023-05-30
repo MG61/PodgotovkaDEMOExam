e8fa5d1f739bb03491334003e27fba9d757a7ee1
Никита Абраменко <abramenko098@gmail.com>
2023-05-31 00:54:46 +0300

    Добавьте файлы проекта.

---
+++ PodgotovkaDEMOExam/Admin.xaml.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.Win32;
+using PodgotovkaDEMOExam.DataSet1TableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PodgotovkaDEMOExam
+{
+
+    public partial class Admin : Window
+    {
+
+        DataSet1 dataSet1 = new DataSet1();
+        ProductTableAdapter PTA = new ProductTableAdapter();
+        OrdersTableAdapter OTA = new OrdersTableAdapter();
+        String global_photo = "";
+        public Admin()
+        {
+            InitializeComponent();
+
+            data.ItemsSource = dataSet1.Product.DefaultView;
+            PTA.Fill(dataSet1.Product);
+            OTA.Fill(dataSet1.Orders);
+        }
+
+        private void Button_add_product(object sender, RoutedEventArgs e)
+        {
+            try { 
+            PTA.InsertQuery(name_product.Text, price_product.Text, quantity_product.Text, "Image/" + global_photo);
+            PTA.Fill(dataSet1.Product);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Не удалось добавить данные!");
+            }
+        }
+
+        private void Button_change_product(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (data.SelectedItem != null)
+                {
+                    DataRowView selItem = (DataRowView)data.SelectedItem;
+                    int id = (int)selItem["ID_product"];
+                    PTA.UpdateQuery(name_product.Text, price_product.Text, quantity_product.Text,"Image/" + global_photo, id);
+                    PTA.Fill(dataSet1.Product);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Не удалось изменить данные!");
+            }
+        }
+
+        private void Button_delete_product(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (data.SelectedItem != null) 
+                { 
+                DataRowView selItem = (DataRowView)data.SelectedItem;
+                int id = (int)selItem["ID_product"];
+                PTA.DeleteQuery(id);
+                PTA.Fill(dataSet1.Product);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Не удалось удалить данные!");
+            }
+        }
+
+
+        private void Button_back(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MainWindow go = new MainWindow();
+                go.Show();
+                this.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Не удалось удалить данные!");
+            }
+        }
+
+        private void Button_add_order(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DataRowView selItem = (DataRowView)data.SelectedItem;
+                String name_order = (String)selItem["Name"];
+                String price_order = (String)selItem["Price"];
+                OTA.InsertQuery(name_order, price_order, "1", price_order);
+                OTA.Fill(dataSet1.Orders);
+                Button_order.Visibility= Visibility.Visible;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Не удалось добавить данные!");
+            }
+        }
+
+        private void Button_order_show(object sender, RoutedEventArgs e)
+        {
+            Order go = new Order();
+            go.ShowDialog();
+        }
+
+        
+        private void openButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            string fileName = "";
+            string name = "";
+            if (fileDialog.ShowDialog() == true)
+            {
+                fileName = fileDialog.FileName;
+                name = fileDialog.SafeFileName;
+                global_photo = name;
+            }
+
+            string projectPath = Directory.GetCurrentDirectory().Replace("\\bin\\Debug\\net6.0-windows", "");
+            string toPath = $"{projectPath}\\Image\\{name}";
+
+            try
+            {
+                File.Copy(fileName, toPath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+    }
+}
---
+++ PodgotovkaDEMOExam/Order.xaml.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using Microsoft.Win32;
+using PodgotovkaDEMOExam.DataSet1TableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PodgotovkaDEMOExam
+{
+    /// <summary>
+    /// Логика взаимодействия для Order.xaml
+    /// </summary>
+    public partial class Order : Window
+    {
+        DataSet1 dataSet1 = new DataSet1();
+        OrdersTableAdapter OTA = new OrdersTableAdapter();
+
+        public Order()
+        {
+            InitializeComponent(); 
+            data.ItemsSource = dataSet1.Orders.DefaultView;
+            OTA.Fill(dataSet1.Orders);
+
+            Summa();
+        }
+        
+        private void Button_plus_order(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (data.SelectedItem != null)
+                {
+                    DataRowView selItem = (DataRowView)data.SelectedItem;
+                    int id = (int)selItem["ID_order"];
+                    String name_order = (String)selItem["Name"];
+                    String price_order = (String)selItem["Price"];
+                    String quantity_order = (String)selItem["Quantity"];
+                    quantity_order = (Convert.ToInt32(quantity_order) + 1).ToString();
+                    String total_price = (Convert.ToInt32(price_order) * Convert.ToInt32(quantity_order)).ToString();
+                    OTA.UpdateQuery(name_order, price_order, quantity_order, total_price, id);
+                    OTA.Fill(dataSet1.Orders);
+                    Summa();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Не удалось изменить данные!");
+            }
+        }
+
+        private void Button_minus_order(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (data.SelectedItem != null)
+                {
+                    DataRowView selItem = (DataRowView)data.SelectedItem;
+                    int id = (int)selItem["ID_order"];
+                    String name_order = (String)selItem["Name"];
+                    String price_order = (String)selItem["Price"];
+                    String quantity_order = (String)selItem["Quantity"];
+                    if (Convert.ToInt32(quantity_order) > 1)
+                    {
+                        quantity_order = (Convert.ToInt32(quantity_order) - 1).ToString();
+                        String total_price = (Convert.ToInt32(price_order) * Convert.ToInt32(quantity_order)).ToString();
+                        OTA.UpdateQuery(name_order, price_order, quantity_order, total_price, id);
+                        OTA.Fill(dataSet1.Orders);
+                        Summa();
+                    }
+                    else
+                    {
+                        OTA.DeleteQuery(id);
+                        OTA.Fill(dataSet1.Orders);
+                        Summa();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Не удалось изменить данные!");
+            }
+        }
+
+        private void Button_delete_order(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (data.SelectedItem != null)
+                {
+                    DataRowView selItem = (DataRowView)data.SelectedItem;
+                    int id = (int)selItem["ID_order"];
+                    OTA.DeleteQuery(id);
+                    OTA.Fill(dataSet1.Orders);
+                    Summa();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Не удалось изменить данные!");
+            }
+        }
+
+        private void Button_buy_order(object sender, RoutedEventArgs e)
+        {
+            //SaveFileDialog saveFileDialog = new SaveFileDialog();
+            //string fileText = "Your output text";
+            //if (saveFileDialog.ShowDialog() == true)
+            //    File.WriteAllText(saveFileDialog.FileName, fileText);
+
+            //Stream stream = File.Open(saveFileDialog.FileName, FileMode.Open);
+            //Stream outputStream = new MemoryStream();
+            //DocumentConverter.ToDocument(stream, outputStream, FileType.DOC_PDF);
+        } 
+        
+        public void Summa()
+        {
+            //Подсчёт суммы
+            int sum = 0;
+            foreach (DataRowView row in data.ItemsSource)
+            {
+                sum += Int32.Parse(row["Total"].ToString());
+            }
+            summ.Text = sum.ToString();
+            discount.Text = ((Int32.Parse(summ.Text) / 100) * 10).ToString();
+            total.Text = ((Int32.Parse(summ.Text) - Int32.Parse(discount.Text))).ToString();
+        }
+    }
+}
